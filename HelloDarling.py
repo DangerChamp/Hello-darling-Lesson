c59632f7fc9e784b0dab2922ecdbcba0854a5283
genesis gir <daintygenesis@gmail.com>
2021-07-12 13:33:26 -0400

    The file is in now

---
+++ HelloDarling.py
@@ -0,0 +1,14 @@
+#This program says hello and asks for a age
+print('Hello!') # saying hello to user
+print() # empty line
+print('whats your name?') # asking the user their name
+name=input()
+print('oh why hello '+name+' its nice to meet darling!') # chatting with user
+print() # empty line
+print('This is how many characters are in your name!') # telling the user how many letters are in their name
+print(len(name)) # the amount of letters in the users name
+print('So how old are you?') # asking for age
+age=input() #input means variable
+print('wow!well guess what you will be ' + str(int(age) + 1) + ' in one year.') #how old they will be in a year
+print() #empty line
+print('goodbye sweetheart!') #saying goodbye to the user  
