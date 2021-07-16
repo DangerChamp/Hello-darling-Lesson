e9c858860bce98e56c0f38edde5bcbad12f839b3
Cristian <daintygenesis@gmail.com>
2021-07-16 16:26:56 -0500

    @ line 14 anded a "you" so it says "its nice to meet you darling" instead

--- HelloDarling.py
+++ HelloDarling.py
@@ -4,32 +4,33 @@ script and get to understand why we do the things i've set for you to learn from
 make sure to read threw and see the comments preset for you to see what each line does!
 it can be intimidating but is quite easy , make sure to follow me on twitch @GenesisGir and on GitHub
 """
-print('Hello!') # This line is saying hello with the print() function
-print() # When you use print() you can create empty lines like this one!
-print('whats your name?') # Asking the user their name with the print() function 
-name=input() # I'm using the input function to take input from the user
-print('oh why hello '+name+' its nice to meet darling!') # Chatting with the user , we inserted the variable that the user set
-print() # Empty line using the print() function
-print('This is how many characters are in your name!') # Telling the user how many letters are in their name
-print(len(name)) # The len function shows the amount of intergers inside the variable
-print('So how old are you?') # Using the print function to type more text on screen
-age=input() # Input function takes users input 
-print('wow! well guess what you will be ' + str(int(age) + 1) + ' in one year.') 
-"""" In line 17 we are using the str , int functions for 2 things. The str function is converting
+print('Hello!') #This line is saying hello with the print() function
+print() # when you use print() you can create empty lines like this one!
+print('whats your name?') # asking the user their name with the print() function 
+name=input() #im using the input function to take input from the user
+print('oh why hello '+name+' its nice to meet you darling!') # chatting with the user , we inserted the variable that the user set
+print() # empty line using the print() function
+print('This is how many characters are in your name!') # telling the user how many letters are in their name
+print(len(name)) # the len function shows the amount of intergers inside the variable
+print('So how old are you?') # using the print function to type more text on screen
+age=input() #input function takes users input 
+print('wow! Well guess what you will be ' + str(int(age) + 1) + ' in one year.') 
+"""" in line 17 we are using the str , int functions for 2 things. the str function is converting
 the interger into a string data type and the int function take the interger and converts 
 floating data types into intergers! and the operators to add a new part of instruction to the command line.
 """
-print() # This empty line is using the print() function to create space   
-print('Goodbye sweetheart!') # Saying goodbye to the user using the print function
+print() #this empty line is using the print() function to create space   
+print('goodbye sweetheart!') #saying goodbye to the user using the print function
 """"
-And that's it! this is simple code that you can review and analyze and study
+And thats it! this is simple code that you can review and analyze and study
 and branch out and make your own as well! make sure to follow me @genesisgir on twitch
-I do endless livestreams of my workflow and how i do things
+i do endless livestreams of my workflow and how i do things
 coding is a world of creativity and diverse scripts that come together that make programs
 below are some links to my sites and where this code was remixed from buh bye!
-Twitch- www.twitch.com/cashbunny2k
-Github- https://github.com/GenesisGir
-Source code -https://automatetheboringstuff.com/2e/chapter1/
-My tumblr-https://www.tumblr.com/blog/view/genesisgir
+twitch- www.twitch.com/cashbunny2k
+github- https://github.com/GenesisGir
+source code -https://automatetheboringstuff.com/2e/chapter1/
+my tumblr-https://www.tumblr.com/blog/view/genesisgir
 """
-# Reminder! Son't rush your learning process, small steps, more power!
+#reminder! dont rush your learning process , small steps , more power!
+
